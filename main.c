37d3d22dc7ecc003a396dd85ea4c36bf40272456
ivanchino96 <scivanxino@gmail.com>
2015-12-24 15:00:22 +0100

    Update main.c
    He cambiado lo de los structs.

--- main.c
+++ main.c
@@ -3,7 +3,14 @@
 #include <stdlib.h>
 #include <windows.h>
 
-void leerArchivo(char* archivo, char* proceso, int *q, int *inicio, int *duracion, int *num_lineas);
+typedef struct {
+  int quanto=-1;
+  char nombre[100]="";
+  int inicio=-1;
+  int duracion=-1;
+} proceso;
+
+void leerArchivo(char* archivo, int* num_lineas, proceso* procesos[]);
 
 
 void main(int argc, char *argv[]) 
@@ -23,23 +30,20 @@ void main(int argc, char *argv[])
 	handle=FindFirstFile(argv[1],&datos);
 	//Si el archivo existe
 	if (handle!=INVALID_HANDLE_VALUE){
-		char* nombrearchivo = (char*)malloc(100*sizeof(char));
+                proceso* procesos = (proceso*)malloc(100*sizeof(proceso));
+                char* nombrearchivo = (char*)malloc(100*sizeof(char));
 		nombrearchivo = argv[1];
 		int contador;
-		char* nombre= (char*)malloc(sizeof(char));
-		int *q = (int*)malloc(sizeof(int));
-		int *inicio = (int*)malloc(100*sizeof(int));
-		int *duracion = (int*)malloc(100*sizeof(int));
 		int *num_lineas=(int*)malloc(sizeof(int));
 		*num_lineas = 0;
 
-		leerArchivo(nombrearchivo,nombre,q,inicio,duracion,num_lineas);
+		leerArchivo(nombrearchivo,num_lineas, procesos);
 
 		//PROVISIONAL PARA DEBUGGING
 		printf("Numero de lineas: %d\n", *num_lineas); //Solamente compruebamos que nos devuelve el numero de lineas del fichero correcto
 
 		for(contador=0; contador<*num_lineas; contador++){
-			printf("Linea %d contiene al proceso %c, con quanto=%d, inicio=%d y duracion=%d \n", contador+1, nombre[contador], q[contador], inicio[contador], duracion[contador]);
+			printf("Linea %d contiene al proceso %s, con quanto=%d, inicio=%d y duracion=%d \n", contador+1, procesos->nombre[contador], procesos->quanto[contador], procesos->inicio[contador], procesos->duracion[contador]);
 		}
 		//ROUNDROBIN
 	}else{
@@ -52,7 +56,7 @@ void main(int argc, char *argv[])
 
 
 //FUNCION PARA QUE LEA DEL ARCHIVO, GUARDES EN VECTORES Y COMPRUEBA QUE SE HAN GUARDADO BIEN
-void leerArchivo(char* archivo, char* proceso, int *q, int *inicio, int *duracion, int *num_lineas)
+void leerArchivo(char* archivo, int* num_lineas, proceso* procesos[])
 {
 	FILE* miarchivo;
 	int contador; 
@@ -71,8 +75,15 @@ void leerArchivo(char* archivo, char* proceso, int *q, int *inicio, int *duracio
 	//En este bucle tengemos en cuenta los campos con la expresion %d:%[A-Z]:%d:%d\n, cada campo lo asigno al vector que le corresponde
 	for(contador = 0; contador<*num_lineas; contador++)
 	{
-		fscanf(miarchivo, "%d:%[A-Z]:%d:%d\n", &q[contador], &proceso[contador], &inicio[contador], &duracion[contador]);
+		fscanf(miarchivo, "%d:%[A-Z]:%d:%d\n", &procesos->quanto[contador], &procesos->nombre[contador], &procesos->inicio[contador], &procesos->duracion[contador]);
 	}
 
+        //Detectamos si falta algun tipo de dato
+        for(contador = 0; contador<*num_lineas; contador++){
+            if(procesos->quanto[contador] == -1) printf("Falta el quanto del proceso %d", contador++);
+            if(procesos->nombre[contador] == "") printf("Falta el nombre del proceso %d", contador++);
+            if(procesos->inicio[contador] == -1) printf("Falta el tiempo de inicio del proceso %d", contador++);
+            if(procesos->duracion[contador] == -1) printf("Falta la duracion del proceso %d", contador++);
+        }
 	fclose(miarchivo);
 }
